ed37be0b94962878e018473fadb0e95c2589a61f
thiago.smessias <thiago.smessias@CAS555104W0725.senacsp.edu.br>
2017-09-20 22:33:36 -0300

    Add angularjs

--- Ecommerce/src/main/webapp/js/app.js
+++ Ecommerce/src/main/webapp/js/app.js
@@ -1,17 +1,20 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 angular.module('AppMain', ["ngRoute"])
-    .config(function($routeProvider) {
-        $routeProvider
-        .when('app',
-                {templateUrl: '/html/Usuarios/Usuarios.html'}
-        );
-    })
-    .controller('AppController', function ($scope) {
-        $scope.message = "Hello World!";
-    });
+.config(function ($routeProvider){
+  $routeProvider
+  .when("/", {
+    temaplteUrl: "/templates/index.html"
+  }).when("/index", {
+    templateUrl: "/templates/index.html"
+  })
+  .otherwise('/');
 
+  $routeProvider.caseInsensitiveMatch = true;
+}).controller("IndexController", function($scope) {
+  $scope.hi = "Hello World";
+});
