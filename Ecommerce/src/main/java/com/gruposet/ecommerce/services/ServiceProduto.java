2f327c74b39c409bc4e1607766c068caad82bedd
Rafael Ferreira <rafaelfs96@outlook.com>
2017-10-09 00:52:43 -0300

    xv

--- Ecommerce/src/main/java/com/gruposet/ecommerce/services/ServiceProduto.java
+++ Ecommerce/src/main/java/com/gruposet/ecommerce/services/ServiceProduto.java
@@ -4,43 +4,21 @@ import com.gruposet.ecommerce.models.Produto;
 
 public abstract class ServiceProduto {
 
-<<<<<<< current
-    public static boolean validaProduto(Produto pro) {
-        return validaEstoque(pro.getEstoque())
-                || validaPalavra(pro.getModelo())
-                || validaPalavra(pro.getMarca())
-                || validaPreco(pro.getPreco());
-    }
+  public static boolean validaProduto(Produto pro) {
+    return validaEstoque(pro.getEstoque()) || validaPalavra(pro.getModelo()) || validaPalavra(pro.getMarca())
+        || validaPreco(pro.getPreco());
+  }
 
-    private static boolean validaPalavra(String palavra) {
-        return "".equals(palavra);
-    }
+  private static boolean validaPalavra(String palavra) {
+    return "".equals(palavra);
+  }
 
-    private static boolean validaEstoque(int estoque) {
-        return estoque < 0;
-    }
+  private static boolean validaEstoque(int estoque) {
+    return estoque < 0;
+  }
 
-    private static boolean validaPreco(double preco) {
-        return preco <= 0;
-=======
-    public static boolean validaProduto(Produto pro){
-        return true;
-    }
+  private static boolean validaPreco(double preco) {
+    return preco <= 0;
 
-    private static boolean validaEstoque(int estoque){
-
-    }
-
-    private static boolean validaModelo(String modelo){
-
-    }
-
-    private static boolean validaMarca(String marca){
-
-    }
-
-    private static boolean validaPreco(double preco){
-
->>>>>>> before discard
-    }
+  }
 }
