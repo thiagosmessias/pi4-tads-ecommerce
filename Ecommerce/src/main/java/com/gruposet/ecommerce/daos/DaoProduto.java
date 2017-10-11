adbdf2ab658b0b41c0ee2c677a809fe17a102268
rafaelfs96 <rafaelfs96@outlook.com>
2017-10-11 20:21:47 -0300

    sdfs

--- Ecommerce/src/main/java/com/gruposet/ecommerce/daos/DaoProduto.java
+++ Ecommerce/src/main/java/com/gruposet/ecommerce/daos/DaoProduto.java
@@ -9,7 +9,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DaoProduto implements InterfaceDao {
-
+//asdasdasd
     private final Database database;
     private Produto produto;
     private ArrayList<Produto> produtos;
